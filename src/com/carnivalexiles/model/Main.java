21b9a27b8ef995b75669f9571059b27debae6075
Craig Kelleher <kelleher.craig@gmail.com>
2022-12-09 23:28:57 -0800

    Added displayTitle and displayinstruction methods to the Welcome Screen Called those methods in main

---
+++ src/com/carnivalexiles/model/Main.java
@@ -0,0 +1,11 @@
+package com.carnivalexiles.model;
+
+import com.carnivalexiles.view.WelcomeScreen;
+
+public class Main {
+  public static void main(String[] args) {
+    //Print opening dialogue
+    WelcomeScreen.displayTitle();
+    WelcomeScreen.displayInstructions();
+  }
+}
---
+++ src/com/carnivalexiles/view/WelcomeScreen.java
@@ -0,0 +1,16 @@
+package com.carnivalexiles.view;
+
+public class WelcomeScreen {
+
+  public static void displayTitle(){
+    System.out.println("Welcome to Carnival Exiles \n\n");
+  }
+  public static void displayInstructions(){
+    System.out.println(
+            "You are on the biggest Carnival cruise ship and you decide to go to the buffet for bacon. \n" +
+            "As you reach for the last piece, a hungover buffoon fights you for the piece and throws you overboard. " +
+            "As you are flailing overboard, you quickly store the crisp smoked applewood bacon into your trusty waterproof fanny pack. \n\n" +
+            "Suddenly, you wake up to a monkey bonking you on the head with a bottle on a mysterious Island all while eating your buffet bacon… \n\n" +
+            "Would you like to start the game? \n");
+  }
+}
