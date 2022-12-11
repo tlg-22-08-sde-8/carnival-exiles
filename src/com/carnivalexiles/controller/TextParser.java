d39b3f242ed9c2e5f3faedae15c3d2b333302017
craigkelleher <kelleher.craig@gmail.com>
2022-12-11 12:27:56 -0800

    Added TODO: Quit Game method for console view Fixed typos in TextParser TODO(s)

--- src/com/carnivalexiles/controller/TextParser.java
+++ src/com/carnivalexiles/controller/TextParser.java
@@ -1,4 +1,4 @@
-package controller;
+package com.carnivalexiles.controller;
 
 public class TextParser {
 
@@ -6,13 +6,13 @@ public class TextParser {
   // TODO: 12/9/2022 If valid then send the match to its named method
   // TODO: 12/9/2022 if (UserAction != null) { }
   // TODO: 12/9/2022 Switch
-  // TODO: 12/9/2022 is useraction == Consume?
-  // TODO: 12/9/2022 is useraction == Go TO?
-  // TODO: 12/9/2022 is useraction == PickUP?
+  // TODO: 12/9/2022 is userAction == Consume?
+  // TODO: 12/9/2022 is userAction == Go TO?
+  // TODO: 12/9/2022 is userAction == PickUP?
 
 
 
-  // TODO: 12/9/2022 Action method perform the action thats matched
+  // TODO: 12/9/2022 Action method perform the action that's matched
 
   // TODO: 12/9/2022 Go To method will send the user to a location
 
--- src/com/carnivalexiles/view/ConsoleView.java
+++ src/com/carnivalexiles/view/ConsoleView.java
@@ -2,7 +2,7 @@ package com.carnivalexiles.view;
 
 public class ConsoleView {
 
-
+// TODO: Quit Game Method: "Type exit or quit to exit the game"
 
 
 }
