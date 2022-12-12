b675a60ea80f2c8915ab6cd22df93128e10275a3
craigkelleher <kelleher.craig@gmail.com>
2022-12-12 00:37:37 -0600

    Comment out Ticket 73 and 75 work

--- src/com/carnivalexiles/controller/TextParser.java
+++ src/com/carnivalexiles/controller/TextParser.java
@@ -10,15 +10,15 @@ public class TextParser {
     //Check user input for Game Initiation
     public static void playGame() throws IOException {
         //Retrieve user Input to play the game until they type 'q'
-        BufferedReader br;
-        String input;
-        br = new BufferedReader(new InputStreamReader(System.in));
-        do {
-            // place game logic code here. When user types q, game quits
-            System.out.print("> ");
-            input = br.readLine();
-            System.out.println("You entered '" + input + "'");
-        } while (!"Quit".equals(input));
+//        BufferedReader br;
+//        String input;
+//        br = new BufferedReader(new InputStreamReader(System.in));
+//        do {
+//            // place game logic code here. When user types q, game quits
+//            System.out.print("> ");
+//            input = br.readLine();
+//            System.out.println("You entered '" + input + "'");
+//        } while (!"Quit".equals(input));
 
     // TODO: 12/9/2022 Parse the initial text and see if its valid
     // TODO: 12/9/2022 If valid then send the match to its named method
