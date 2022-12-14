d3c822930294af8a7818b4a7845ebf5ac77bfbaa
gmejia3 <92178130+gmejia3@users.noreply.github.com>
2022-12-14 15:37:28 -0800

    All data is now extractable from the json file. AB#86

--- src/com/carnivalexiles/controller/JsonParser.java
+++ src/com/carnivalexiles/controller/JsonParser.java
@@ -9,6 +9,7 @@ import com.google.gson.reflect.TypeToken;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class JsonParser {
@@ -19,7 +20,6 @@ public class JsonParser {
 
       //Create a Gson
       Gson gson = new Gson();
-
 
 
       //Method 1
@@ -41,9 +41,6 @@ public class JsonParser {
 //
 //      }
 
-
-
-
       //Method 2
       //Pulling in the file into json.
       JsonElement fileElement = com.google.gson.JsonParser.parseReader( new FileReader("resources/LocationData.json"));
@@ -53,7 +50,7 @@ public class JsonParser {
 //      String something = fileObject.get("option2").getAsString();
 //      System.out.println("Start Object from Json: " + something);
 
-      JsonArray jsonArrayOfLocation = fileObject.get("start").getAsJsonArray();
+      JsonArray jsonArrayOfLocation = fileObject.get("Coco Forest").getAsJsonArray();
 
       //String[] itemList = new String[];
       for (JsonElement locationElement : jsonArrayOfLocation) {
@@ -61,26 +58,34 @@ public class JsonParser {
         JsonObject locationJsonObject = locationElement.getAsJsonObject();
 
 
-
-        String[]itemList = {};
-        //ArrayofLocations as a string.
-        //Object to hold the array of locationobjects.
+        // TODO: 12/14/2022 ArrayofLocations as a string.
+        // TODO: 12/14/2022 Object to hold the array of locationobjects.
 
         //Extract Data
+
+
+
+
+
+        var listOfItems = new ArrayList<String>();
+        for(JsonElement items: locationJsonObject.get("items").getAsJsonArray()) {
+
+          listOfItems.add(String.valueOf(items));
+        }
+        String[]itemList = listOfItems.toArray(new String[listOfItems.size()]);
+
         String name = locationJsonObject.get("name").getAsString();
-        //itemList = (locationJsonObject.get("items").getAsJsonArray());
-        String visibleLocations = locationJsonObject.get("items").getAsString();
+        String visibleLocations = locationJsonObject.get("visibleLocations").getAsString();
         String description = locationJsonObject.get("description").getAsString();
 
         Location location = new Location(description, name, itemList, visibleLocations);
-        System.out.println(location.getVisibleLocations());
-        System.out.println(location.getName());
+//        System.out.println(location.getVisibleLocations());
+//        System.out.println(location.getName());
+//        System.out.println(Arrays.toString(location.getItems()));
+//        System.out.println(location.getDescription());
+        System.out.println(location);
+
       }
-
-
-
-
-
 
       try {
         reader.close();
