ceb03a1aae5be48c366656e3b94dbf2aaeeb1298
Craig Kelleher <kelleher.craig@gmail.com>
2022-12-19 14:01:41 -0800

    AB#556 Implement cry action into the game

--- src/main/java/com/carnivalexiles/controller/TextParser.java
+++ src/main/java/com/carnivalexiles/controller/TextParser.java
@@ -303,7 +303,9 @@ public class TextParser {
     }
 
     private static void cry() throws IOException, InterruptedException {
-        System.out.println("STUB METHOD - IMPLEMENT DURING SPRINT 3");
+        System.out.println("In a fit of despair, you have a good cry and feel slightly better");
+        user.modifyHealthPoints(5);
+        day.increaseTimeOfDay(1);
         pauseTheGame();
         clearScreen();
         playGame(user, consoleView.getCurrentLocation(), day);
