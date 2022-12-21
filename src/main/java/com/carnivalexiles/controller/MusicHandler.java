7b0fde70a735d80738e3e46f0acbf95d71cd38c8
Philip Son <sonp1998@gmail.com>
2022-12-21 12:42:42 -0500

    merged with dev, formatted few lines with 4 spaces...

--- src/main/java/com/carnivalexiles/controller/MusicHandler.java
+++ src/main/java/com/carnivalexiles/controller/MusicHandler.java
@@ -12,58 +12,60 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 
 public class MusicHandler {
 
-  //fields
-  private static boolean music = true;
-  private static Clip clip;
+    //fields
+    private static boolean music = true;
+    private static Clip clip;
 
 
-  public static boolean isMusic() {
-    return music;
-  }
+    public static boolean isMusic() {
+        return music;
+    }
 
-  public static boolean setMusic(boolean music) {
-    MusicHandler.music = music;
-    return music;
-  }
+    public static boolean setMusic(boolean music) {
+        MusicHandler.music = music;
+        return music;
+    }
 
-  public MusicHandler() {
-  }
+    public MusicHandler() {
+    }
 
-  public static void music() throws LineUnavailableException {
-    if (music) {
-      try {
-        URL musicStream = MusicHandler.class.getClassLoader().getResource("island-beach.wav");
-        if (musicStream == null) {
-          throw new IllegalArgumentException("file not found");
+    public static void music() throws LineUnavailableException {
+        if (music) {
+            try {
+                URL musicStream = MusicHandler.class.getClassLoader()
+                        .getResource("island-beach.wav");
+                if (musicStream == null) {
+                    throw new IllegalArgumentException("file not found");
+                }
+                AudioInputStream inputStream = AudioSystem.getAudioInputStream(musicStream);
+                clip = AudioSystem.getClip();
+                clip.open(inputStream);
+
+                FloatControl gainControl = (FloatControl) clip.getControl(
+                        FloatControl.Type.MASTER_GAIN);
+                gainControl.setValue(-25.0f); // Reduce volume by 20 decibels.
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+                clip.start();
+            } catch (UnsupportedAudioFileException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (LineUnavailableException e) {
+                e.printStackTrace();
+            }
         }
-        AudioInputStream inputStream = AudioSystem.getAudioInputStream(musicStream);
-        clip = AudioSystem.getClip();
-        clip.open(inputStream);
+        if (setMusic(false)) {
+            clip.stop();
+        }
+    }
 
-        FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-        gainControl.setValue(-25.0f); // Reduce volume by 20 decibels.
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
-        clip.start();
-      } catch (UnsupportedAudioFileException e) {
-        e.printStackTrace();
-      } catch (IOException e) {
-        e.printStackTrace();
-      } catch (LineUnavailableException e) {
-        e.printStackTrace();
-      }
+    private static InputStream getFileFromResourceAsStream(String fileName) {
+        ClassLoader classLoader = MusicHandler.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
     }
-    if (setMusic(false)) {
-      clip.stop();
-    }
-  }
-
-  private static InputStream getFileFromResourceAsStream(String fileName) {
-    ClassLoader classLoader = MusicHandler.class.getClassLoader();
-    InputStream inputStream = classLoader.getResourceAsStream(fileName);
-    if (inputStream == null) {
-      throw new IllegalArgumentException("file not found! " + fileName);
-    } else {
-      return inputStream;
-    }
-  }
 }
